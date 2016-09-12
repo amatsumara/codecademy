1b4f3c1af3e2a854a956887840d60b1ed3c4da05
Alex K <alex123@hades.in.ua>
2016-09-12 13:45:12 +0300

    first commit

---
+++ codecademy.pl
@@ -0,0 +1,50 @@
+#
+
+# Using recursion instead of strings
+def digit_sum(x):
+    summ = 0
+    if x // 10 <> 0:
+        print "Going deeper..."
+        summ = digit_sum(x // 10)
+    print "Going out",
+    summ += x % 10
+    print summ
+    return summ
+
+# Recursion instead of loop
+def factorial(x):
+    f = 1
+    if x > 1:
+        f = factorial(x-1)
+    elif x == 0:
+        f = 1
+        return f
+    f = f * x
+    return f
+
+def is_prime(x):
+    if x < 2:
+        return False
+    elif x == 2:
+        return True
+    else:
+        # for x==3 this loop doesn't executes
+        for n in range(2, x - 1):
+            if (x % n == 0):
+                print "false"
+                return False
+        else:
+            print "true"
+            return True
+
+def main():
+    x = 3
+    #print digit_sum(x)
+    #print factorial(x)
+    print is_prime(x)
+
+
+if __name__ == "__main__":
+    main()
+
+
